4a88f1fc86c3ff57963bbf4b7f7b6b17b2e2e426
jacklord21 <giacomo.matteucci@studenti.unicam.it>
2023-12-09 16:12:46 +0100

    Fixed SplashScreen
    The previous SplashScreen had been implemented by Unity settings, those common to any project.
    Now, however, those settings have been eliminated (making only the Unity logo appear) to make way for a new SplashScreen, implemented through a scene (the first one that will be started in the game.
    The motivation for this lies in the fact that using the scene makes it possible to build the desired animations (as done here).

---
+++ Assets/Scripts/FirstSceneUIController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstSceneUIController : MonoBehaviour
+{
+    [SerializeField]
+    public String animationName;
+    [SerializeField]
+    public float animationSpeed;
+    private Animator animator;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.speed = (animationSpeed==0 ? 0.2f : animationSpeed);
+        animator.Play(animationName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
