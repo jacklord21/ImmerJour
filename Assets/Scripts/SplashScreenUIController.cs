f4936ebd7fe5b78341f4b99b3549d1613479773c
jacklord21 <giacomo.matteucci@studenti.unicam.it>
2023-12-19 11:40:51 +0100

    Renamed script controller for splash screen

--- Assets/Scripts/SplashScreenUIController.cs
+++ Assets/Scripts/SplashScreenUIController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstSceneUIController : MonoBehaviour
+public class SplashScreenUIController : MonoBehaviour
 {
     [SerializeField]
     public String animationName;
